ddabfa3bb9ad1d088a63c4202e69e1f90db091e2
mhmmdj <chamaleki@gmail.com>
2022-05-27 13:16:26 +0430

    first commit

---
+++ API/Entities/User.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
